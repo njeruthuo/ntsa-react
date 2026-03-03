944af550f3181ad01c7d07e9748a538d64d67fad
njeruthuo <juliusn411@gmail.com>
2026-03-03 15:50:18 +0300

    fix: added dashboard date formatting

---
+++ src/utils/dateFormater.ts
@@ -0,0 +1,13 @@
+export const formatDate = (dateInput: string | Date) => {
+  const date = new Date(dateInput);
+
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  }).format(date);
+};
